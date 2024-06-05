4c49a55cc6d9583dda202b7c1a88a23680d8b55e
Uday Pratap Chauhan <96443357+udc29h@users.noreply.github.com>
2024-06-06 02:17:14 +0530

    Add routing and it's intial saving point everything working fine till now :)

---
+++ zcom/src/App.js
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+// import { Counter } from './features/counter/Counter';
+
+import './App.css';
+import Home from './pages/Home';
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:(<Home></Home>
+    ),
+  },{
+    path:"/login",
+    element:(<LoginPage></LoginPage>)
+  },{
+    path:"/signup",
+    element:(<SignupPage></SignupPage>)
+  }
+])
+function App() {
+  return (
+    <div className="App">
+       <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ zcom/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../features/navbar/Navbar'
+import ProductList from '../features/product-list/ProductList'
+
+const Home = () => {
+  return (
+    <div>
+    <NavBar>
+        <ProductList></ProductList>
+    </NavBar>
+    </div>
+  )
+}
+
+export default Home
